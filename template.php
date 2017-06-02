5f04e6e0f70175026f55ec971fe1041e4e82b28a
SBettis <SarahSBettis@gmail.com>
2017-06-02 11:55:36 -0700

    Add files via upload

---
+++ template.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once 'includes/header.php';
+
+?>
+
+<main>
+    <h2>H2 Header</h2>
+       
+        <p>Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph</p>
+        
+        
+        
+   </main>
+           <div class="sidebar">
+           <p>Portland Weather map</p>
+               <p>Social Media Icons</p>
+            </div>
+
+<?php
+  <?php if ($note=='success') {
+        echo "<h1>Form submitted successfully</h1>";
+}
+    ?>
+
+include_once 'includes/footer.php';
+
+?>
+ 
+    </body>
+</html>
